75fc4e12c4eb0b85160f77431db77bc239ea41e9
FarisKurniawan <marselkurniawan345@gmail.com>
2023-05-10 21:07:49 +0700

    first commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports ={
+    theme: {
+        colors: {
+            'black' : '#000000',
+            'cammie' : '#000b76',
+        },
+        extend: {
+            backgroundImage: {
+                'skil' : "url('../img/sun-tornado.svg')",
+            },
+        },
+    },
+}
+
